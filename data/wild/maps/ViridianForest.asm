1b80628813616cdc9308379c4c5ed4386a1c0931
CrispyBuns <crispybuns2003@gmail.com>
2025-09-28 13:30:18 -0500

    Change ViridianForest Encounters

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,11 +1,11 @@
 ViridianForestWildMons:
 	def_grass_wildmons 9 ; encounter rate
-	db  4, GENTLARVA
-	db  4, GENTLARVA
 	db  4, KOTORA
-	db  5, CATERPIE
+	db  4, MAGIKARP
+	db  4, KOTORA
+	db  5, MAGIKARP
 	db  5, GENTLARVA
-	db  5, WEEDLE
+	db  5, MAGIKARP
 	db  5, KOTORA
 	db  7, KOTORA
 	db  5, KOTORA ; A replacement for the early starter Pikachu. I think this is an amazing compromise that helps actualise Kotora's "mascot status" when it comes to prototype Pokemon.
