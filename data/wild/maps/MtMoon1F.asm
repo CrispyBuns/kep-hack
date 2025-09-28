ebac780609ae45bc7324c8da58674a143f4ef23e
CrispyBuns <crispybuns2003@gmail.com>
2025-09-28 11:57:58 -0500

    Personalize Pokemon Spawn Rates

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -2,14 +2,14 @@ MtMoon1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db  8, BITTYBAT
 	db  7, BITTYBAT
-	db  9, BITTYBAT
+	db  9, GEODUDE
 	db  8, GEODUDE
 	db  6, BITTYBAT
 	db  9, SANDSHREW
 	db  9, GEODUDE
 	db  8, PARAS
 	db 10, BITTYBAT
-	db  8, CLEFAIRY
+	db  8, GASTLY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,15 +1,15 @@
 ViridianForestWildMons:
 	def_grass_wildmons 8 ; encounter rate
-	db  4, CATERPIE
-	db  4, WEEDLE
+	db  4, GENTLARVA
+	db  4, GENTLARVA
 	db  4, PIDGEY
 	db  5, CATERPIE
 	db  5, GENTLARVA
 	db  5, WEEDLE
-	db  7, KAKUNA
-	db  7, METAPOD
+	db  5, KOTORA
+	db  7, KOTORA
 	db  5, KOTORA ; A replacement for the early starter Pikachu. I think this is an amazing compromise that helps actualise Kotora's "mascot status" when it comes to prototype Pokemon.
-	db  9, PIDGEOTTO
+	db  5, KOTORA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
