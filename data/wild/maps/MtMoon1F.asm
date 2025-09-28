61aad9b3607f92fc95f0b5757d25f9feff19df00
CrispyBuns <crispybuns2003@gmail.com>
2025-09-28 12:02:50 -0500

    Make Gastly & Geodude slightly more common in Mt Moon Floor 1

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,11 +1,11 @@
 MtMoon1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  8, BITTYBAT
+	db  8, GASTLY
 	db  7, BITTYBAT
 	db  9, GEODUDE
 	db  8, GEODUDE
 	db  6, BITTYBAT
-	db  9, SANDSHREW
+	db  9, GEODUDE
 	db  9, GEODUDE
 	db  8, PARAS
 	db 10, BITTYBAT
