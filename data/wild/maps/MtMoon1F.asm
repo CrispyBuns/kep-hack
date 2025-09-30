50cd8fbf31e145c8df8ee157127cdac838e247f6
CrispyBuns <crispybuns2003@gmail.com>
2025-09-30 10:43:43 -0500

    Change encounter rates for Forest and Mt Moon 1F and bottom floor 1

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,15 +1,15 @@
 MtMoon1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  8, GASTLY
-	db  7, GASTLY
-	db  9, GEODUDE
+	db  8, BITTYBAT
+	db  7, BITTYBAT
+	db  9, BITTYBAT
 	db  8, GEODUDE
 	db  6, BITTYBAT
+	db  9, SANDSHREW
 	db  9, GEODUDE
-	db  9, GEODUDE
-	db  8, SANDSHREW
-	db 10, BITTYBAT
-	db  8, GASTLY
+	db  8, PARAS
+	db 7, GEODUDE
+	db  8, GEODUDE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,15 +1,15 @@
 ViridianForestWildMons:
-	def_grass_wildmons 9 ; encounter rate
-	db  4, KOTORA
-	db  4, MAGIKARP
-	db  4, KOTORA
-	db  5, MAGIKARP
-	db  5, GENTLARVA
-	db  5, MAGIKARP
+	def_grass_wildmons 8 ; encounter rate
+	db  6, KOTORA
 	db  5, KOTORA
+	db  4, PIDGEY
+	db  6, BLASTYKE
+	db  6, BLASTYKE	
+	db  5, BLASTYKE	
 	db  7, KOTORA
+	db  7, METAPOD
 	db  5, KOTORA ; A replacement for the early starter Pikachu. I think this is an amazing compromise that helps actualise Kotora's "mascot status" when it comes to prototype Pokemon.
-	db  5, KOTORA
+	db  6, KOTORA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
